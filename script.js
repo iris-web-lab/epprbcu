68b2ed898870d1d67db69781449293bb339a1b11
Luka Djordjevic <lukadordevi@gmail.com>
2023-08-23 15:26:00 +0200

    php doesnt work reworkwd with js and a library

---
+++ script.js
@@ -0,0 +1,21 @@
+const feedly = require('feedly');
+
+const feedUrl = 'https://www.example.com/rss.xml';
+
+const feed = feedly.createFeed(feedUrl);
+
+feed.on('load', () => {
+    const items = feed.items;
+
+    const ul = document.createElement('ul');
+
+    for (const item of items) {
+        const li = document.createElement('li');
+        li.textContent = item.title;
+        ul.appendChild(li);
+    }
+
+    document.getElementById('feed').appendChild(ul);
+});
+
+feed.fetch();
