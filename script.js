30587eb9db28e1570cb0afa5a49a1fa08c14486a
Luka Djordjevic <lukadordevi@gmail.com>
2023-10-28 13:16:33 +0200

    hgj

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-const feedUrl = 'https://blog.google/rss/';
+const feedUrl = document.getElementById("feed-url").value;
+const feedItemsList = document.getElementById("feed-items");
 
-const feed = new Nanofeed(feedUrl);
-
-feed.on('update', () => {
-    const items = feed.items;
-
-    const ul = document.createElement('ul');
-
-    for (const item of items) {
-        const li = document.createElement('li');
-        li.textContent = item.title;
-        ul.appendChild(li);
+fetch(feedUrl)
+  .then(response => response.json())
+  .then(data => {
+    for (const item of data.items) {
+      const li = document.createElement("li");
+      li.textContent = item.title;
+      feedItemsList.appendChild(li);
     }
-
-    document.getElementById('feed').appendChild(ul);
-});
-
-feed.fetch();
+  });
